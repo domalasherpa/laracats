f08747f3b55fe3d76bcd9a0f306243bea40b0f1b
Domala <domalasherpa9813@gmail.com>
2023-06-19 10:49:33 +0545

    routing refactored

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 
-require "functions.php";
 $folder = '/laracats';
 
-$uri = $_SERVER['REQUEST_URI'];
+require "functions.php";
+require "router.php";
 
-switch($uri){
-    case $folder.'/':
-        require 'Controllers/i.php';
-        break;
-    
-    case $folder.'/about':
-        require 'Controllers/about.php';
-        break;
- 
-    case $folder.'/contact':
-        require 'Controllers/contact.php';
-        break;
-}
 
 
