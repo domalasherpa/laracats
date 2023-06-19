a04736292ea416d272cb4158648d9cc1424999b2
Domala <domalasherpa9813@gmail.com>
2023-06-19 09:45:57 +0545

    nav link reference modified

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,9 +8,9 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="i.php" class="<?= urlIs('/laracats/i.php') ? 'bg-gray-900 text-white':  'text-gray-300'; ?>  rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="about.php" class="<?= urlIs('/laracats/about.php') ? 'bg-gray-900 text-white': 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">About us</a>
-              <a href="contact.php" class="<?= urlIs('/laracats/contact.php') ? 'bg-gray-900 text-white': 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+              <a href="<?=$folder?>/" class="<?= urlIs('/laracats/i.php') ? 'bg-gray-900 text-white':  'text-gray-300'; ?>  rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="about" class="<?= urlIs('/laracats/about.php') ? 'bg-gray-900 text-white': 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white  rounded-md px-3 py-2 text-sm font-medium">About us</a>
+              <a href="contact" class="<?= urlIs('/laracats/contact.php') ? 'bg-gray-900 text-white': 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
               
             </div>
           </div>
