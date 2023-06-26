f2a77254eea075bbbeeecb30a3e2fee533d0656c
Domala <domalasherpa9813@gmail.com>
2023-06-26 23:00:30 +0545

    minor refactor

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 $folder = '/laracats';
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+require "Database.php";
 
+//connect to mysql database 
 
+$config = require('config.php');
+$db = new Database($config['database']);
+$posts =  $db->query('select * from post')->fetchAll();
 
+foreach ($posts as $post){
+    echo "<li>" . $post['title'] . "</li>";
+}
